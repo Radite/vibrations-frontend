398d1fadb1bb868d44a6378adc0cbb308f1614ad
Radite <joshuameghoo@gmail.com>
2026-02-09 21:00:36 -0500

    Update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import EventAlert from './EventAlert';
 
 const Layout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
